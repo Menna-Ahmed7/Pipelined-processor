4c4cfff38a3db446adf38820eb8086d806032e3d
Sara Gamal <sarah.gerges01@eng-st.cu.edu.eg>
2023-11-28 21:59:27 +0200

    memory

--- fetch.vhd
+++ fetch.vhd
@@ -1,44 +1,46 @@
 USE work.my_pkg.ALL;
+USE work.my_pkg1.ALL;
 
 LIBRARY ieee;
 USE ieee.std_logic_1164.ALL;
-use ieee.numeric_std.all;
-use IEEE.STD_LOGIC_UNSIGNED.ALL;
-
-
+USE ieee.numeric_std.ALL;
+USE IEEE.STD_LOGIC_UNSIGNED.ALL;
 ENTITY fetch IS
     PORT (
-        ram : IN memory_array(0 TO 4095)(15 DOWNTO 0); 
-        pc : in STD_LOGIC_VECTOR(31 DOWNTO 0);
+        registers : IN register_array(0 TO 6)(31 DOWNTO 0);
+        ram : IN memory_array(0 TO 4095)(15 DOWNTO 0);
+        pc : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
+        jz : IN STD_LOGIC;
+        jz_address : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
         instruction : OUT STD_LOGIC_VECTOR (31 DOWNTO 0);
-        jmporcall: out std_logic ;
-        address: out std_logic_vector(31 downto 0)
+        next_pc : OUT STD_LOGIC_VECTOR(31 DOWNTO 0)
     );
 END Fetch;
 ARCHITECTURE arch_fetch OF fetch IS
-signal one:std_logic_vector(31 downto 0);
-signal sel:std_logic;
+    SIGNAL one : STD_LOGIC_VECTOR(31 DOWNTO 0);
+    SIGNAL reg : STD_LOGIC_VECTOR (2 DOWNTO 0);
+    SIGNAL two : STD_LOGIC_VECTOR(31 DOWNTO 0);
 
 BEGIN
-one<=(0=>'1', Others => '0');
-
+    one <= (0 => '1', OTHERS => '0');
+    two <= (1 => '1', OTHERS => '0');
     PROCESS (pc) IS
     BEGIN
-            instruction(31 downto 16) <= ram(to_integer(unsigned(pc)));
-            instruction(15 downto 0) <= ram(to_integer(unsigned(pc+one)));
-            if instruction(31 downto 22) = "100000001" or instruction(31 downto 22)= "100000010" then 
-                    jmporcall<='1';
-                    address<=instruction(21 downto 19);
-                
-            else 
-                 jmporcall<='0';
-                address<=(others=>'0');
+        instruction(31 DOWNTO 16) <= ram(to_integer(unsigned(pc)));
+        instruction(15 DOWNTO 0) <= ram(to_integer(unsigned(pc + one)));
+        IF instruction(31 DOWNTO 22) = "100000001" OR instruction(31 DOWNTO 22) = "100000010" THEN
+            reg <= instruction(21 DOWNTO 19);
+            next_pc <= registers(to_integer(unsigned(reg)));
 
-                end if;
-
-
-
+        ELSIF jz = '1' THEN
+            reg <= (OTHERS => '0');
+            next_pc <= jz_address;
+        ELSIF instruction(31 DOWNTO 22) = "111111111" OR instruction(31 DOWNTO 22) = "111111111" THEN
+            reg <= (OTHERS => '0');
+            next_pc <= (OTHERS => '0');
+        ELSE
+            reg <= (OTHERS => '0');
+            next_pc <= pc + two;
+        END IF;
     END PROCESS;
-
-
 END ARCHITECTURE;
--- memory.vhd
+++ memory.vhd
@@ -13,9 +13,10 @@ USE std.textio.ALL;
 ENTITY memory IS
     PORT (
         ram : INOUT memory_array(0 TO 4095)(15 DOWNTO 0);
-        address : IN STD_LOGIC_VECTOR(19 DOWNTO 0);
-        datain1 : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
-        datain2 : IN STD_LOGIC_VECTOR(15 DOWNTO 0);
+        EA : IN STD_LOGIC_VECTOR(19 DOWNTO 0);
+        datain : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
+        pc : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
+        CCR : INOUT STD_LOGIC_VECTOR(2 DOWNTO 0);
         memory_write : IN STD_LOGIC;
         memory_read : IN STD_LOGIC;
         rti : IN STD_LOGIC;
@@ -23,9 +24,11 @@ ENTITY memory IS
         call : IN STD_LOGIC;
         pop : IN STD_LOGIC;
         push : IN STD_LOGIC;
-        -- pc : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
-        -- sp : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
+
+        sp : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
+        next_pc : OUT STD_LOGIC_VECTOR(31 DOWNTO 0);
         dataout : OUT STD_LOGIC_VECTOR(31 DOWNTO 0)
+
     );
 END memory;
 ARCHITECTURE arch_memory OF memory IS
@@ -42,29 +45,90 @@ ARCHITECTURE arch_memory OF memory IS
     END COMPONENT;
 
     SIGNAL one : STD_LOGIC_VECTOR(11 DOWNTO 0);
+    SIGNAL two : STD_LOGIC_VECTOR(11 DOWNTO 0);
+    SIGNAL written_address : STD_LOGIC_VECTOR(19 DOWNTO 0);
+    SIGNAL written_address1 : STD_LOGIC_VECTOR(19 DOWNTO 0);
+    SIGNAL written_address2 : STD_LOGIC_VECTOR(19 DOWNTO 0);
+    SIGNAL written_data1 : STD_LOGIC_VECTOR(31 DOWNTO 0);
+    SIGNAL written_data2 : STD_LOGIC_VECTOR(15 DOWNTO 0);
+    SIGNAL dataout2 : STD_LOGIC_VECTOR(15 DOWNTO 0);
+    SIGNAL memory_write3_sel : STD_LOGIC;
+    SIGNAL memory_read3_sel : STD_LOGIC;
+    SIGNAL temp : STD_LOGIC;
+    SIGNAL datatemp : STD_LOGIC;
 
 BEGIN
+    one <= (1 => '1', OTHERS => '0');
+    two <= (2 => '1', OTHERS => '0');
+
     -- memory stage
-    memory_instance_1 : memory_access PORT MAP(ram, memory_write, memory_read, written_address, written_data(31 DOWNTO 16), dataout(31 DOWNTO 16));
-    memory_instance_2 : memory_access PORT MAP(ram, memory_write, memory_read, written_address + one, written_data(15 DOWNTO 0), dataout(15 DOWNTO 0));
+    memory_instance_1 : memory_access PORT MAP(ram, memory_write, memory_read, written_address(11 DOWNTO 0), written_data1(31 DOWNTO 16), dataout(31 DOWNTO 16));
+    memory_instance_2 : memory_access PORT MAP(ram, memory_write, memory_read, written_address1(11 DOWNTO 0), written_data1(15 DOWNTO 0), dataout(15 DOWNTO 0));
+    memory_instance_3 : memory_access PORT MAP(ram, memory_write3_sel, memory_read3_sel, written_address2(11 DOWNTO 0), written_data1(15 DOWNTO 0), dataout2(15 DOWNTO 0));
 
     memory : PROCESS
     BEGIN
-        --data mux
-        IF (call = '1') THEN
-            written_data <= pc;
-        ELSE
-            written_data <= datain;
-        END IF;
+        written_address <= (OTHERS => '0');
+        written_address1 <= (OTHERS => '0');
+        written_address2 <= (OTHERS => '0');
+        written_data1 <= (OTHERS => '0');
+        written_data2 <= (OTHERS => '0');
+        dataout2 <= (OTHERS => '0');
+        memory_write3_sel <= '0';
+        memory_read3_sel <= '0';
 
         --address mux selecto
-        address_sel <= ret OR rti OR call OR push OR pop;
+        temp <= ret OR rti OR call OR push OR pop;
 
         --address mux
-        IF (address_sel = '1') THEN
-            written_address <= sp(11 DOWNTO 0);
+        IF (temp = '1') THEN
+            written_address <= sp(19 DOWNTO 0);
         ELSE
-            written_address <= address (11 DOWNTO 0);
+            written_address <= EA (19 DOWNTO 0);
+        END IF;
+
+        IF (call = '1') THEN
+            written_data1 <= pc;
+            written_data2 <= "0000000000000" & CCR;
+            memory_write3_sel <= '1';
+            memory_read3_sel <= '0';
+            written_address1 <= written_address - one;
+            written_address2 <= written_address - two;
+
+        ELSIF (push = '1') THEN
+            written_data1 <= datain;
+            written_data2 <= "0000000000000" & CCR;
+            memory_write3_sel <= '0';
+            memory_read3_sel <= '0';
+            written_address1 <= written_address - one;
+            written_address2 <= written_address - two;
+        ELSIF (pop = '1') THEN
+            written_data1 <= datain;
+            written_data2 <= "0000000000000" & CCR;
+            memory_write3_sel <= '0';
+            memory_read3_sel <= '1';
+            written_address <= written_address + one;
+            written_address1 <= written_address + two;
+            written_address2 <= written_address1 + one;
+        ELSIF (ret = '1') THEN
+            written_data1 <= datain;
+            written_data2 <= "0000000000000" & CCR;
+            memory_write3_sel <= '0';
+            memory_read3_sel <= '1';
+            written_address <= written_address + one;
+            written_address1 <= written_address + two;
+            written_address2 <= written_address1 + one;
+            next_pc <= dataout;
+        ELSIF (rti = '1') THEN
+            written_data1 <= datain;
+            written_data2 <= "0000000000000" & CCR;
+            memory_write3_sel <= '0';
+            memory_read3_sel <= '1';
+            written_address <= written_address + one;
+            written_address1 <= written_address + two;
+            written_address2 <= written_address1 + one;
+            next_pc <= dataout;
+            CCR <= dataout2(2 DOWNTO 0);
         END IF;
         WAIT;
     END PROCESS memory;
