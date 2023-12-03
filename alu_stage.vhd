8f2fadbae96b78e0c83fcb7e300d753bb2d9b0eb
Mennatallah Ahmed <mennaahmed0701@gmail.com>
2023-12-03 18:20:33 +0200

    Update alu_stage.vhd

--- alu_stage.vhd
+++ alu_stage.vhd
@@ -1,12 +1,15 @@
+
 LIBRARY ieee;
 USE ieee.std_logic_1164.ALL;
 
 ENTITY alu_stage IS
     PORT (
+        clk: in std_logic;
         src1, src2, imm, write_back_data, result_in : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
         forward_unit_signal1, forward_unit_signal2 : IN STD_LOGIC_VECTOR (1 DOWNTO 0);
-        imm_signal : IN STD_LOGIC;
+        imm_signal,out_signal : IN STD_LOGIC;
         ALU_sig : IN STD_LOGIC_VECTOR (3 DOWNTO 0);
+        out_port: out std_logic_vector (31 downto 0);
         result_alu : OUT STD_LOGIC_VECTOR (31 DOWNTO 0);
         flags_alu : OUT STD_LOGIC_VECTOR (3 DOWNTO 0)
     );
@@ -26,6 +29,7 @@ ARCHITECTURE arch_alu_stage OF alu_stage IS
 
     COMPONENT alu IS
         PORT (
+            clk: in std_logic;
             src1, src2 : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
             ALU_signal : IN STD_LOGIC_VECTOR (3 DOWNTO 0);
             result : OUT STD_LOGIC_VECTOR (31 DOWNTO 0);
@@ -34,6 +38,7 @@ ARCHITECTURE arch_alu_stage OF alu_stage IS
     END COMPONENT;
 
 BEGIN
+    out_port<= src2 when out_signal='1';
     tmp <= src2 WHEN imm_signal = '0' ELSE
         imm WHEN imm_signal = '1';
     obj1 : mux_3bits PORT MAP(
@@ -42,7 +47,7 @@ BEGIN
     obj2 : mux_3bits PORT MAP(
         tmp, result_in, write_back_data, forward_unit_signal2, src2_alu
     );
-    obj3 : alu PORT MAP(
+    obj3 : alu PORT MAP( clk,
         src1_alu, src2_alu, ALU_sig, result_alu, flags_alu
     );
 END ARCHITECTURE;
